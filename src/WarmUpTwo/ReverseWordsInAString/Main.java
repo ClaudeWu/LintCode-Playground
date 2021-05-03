b6a92da7efc2101b94dcb29da72167f1415ce3c7
Claude Wu <dwu1026@gmail.com>
2021-05-03 01:21:50 -0500

    :lion: update solution of 53.Reverse Words in a String

---
+++ src/WarmUpTwo/ReverseWordsInAString/Main.java
@@ -0,0 +1,13 @@
+package WarmUpTwo.ReverseWordsInAString;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        String test1 = "the sky is blue";
+        String test2 = "hello world";
+
+        System.out.println(s.reverseWords(test1));
+        System.out.println(s.reverseWords(test2));
+    }
+}
