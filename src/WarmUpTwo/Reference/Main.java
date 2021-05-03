1bdc05c77be338f8892791fa1d45d82d67d26689
Claude Wu <dwu1026@gmail.com>
2021-05-03 18:01:00 -0500

    :cow: update solution of 456.Reference

--- src/WarmUpTwo/Reference/Main.java
+++ src/WarmUpTwo/Reference/Main.java
@@ -1,4 +1,4 @@
-package SectionOnePrc.Reference;
+package WarmUpTwo.Reference;
 
 public class Main {
     public static void main(String[] args) {
--- src/WarmUpTwo/Reference/Solution.java
+++ src/WarmUpTwo/Reference/Solution.java
@@ -1,12 +1,33 @@
-package SectionOnePrc.Reference;
+package WarmUpTwo.Reference;
 
-/*
- * LintCode 456. Reference
+/**
+ * LintCode 456.Reference
+ * Link: https://www.lintcode.com/problem/454/
+ *
  * Implement the class ReferenceManager. Include the following two methods:
  * 1. copyValue(Node obj). This would just copy the value from parameter obj to the public attribute node.
- * But obj and node are still two difference instances / objects.
+ *    But obj and node are still two difference instances / objects.
  * 2. copyReference(Node obj). This would copy the reference from obj to node.
- * So that both node and obj are point to the same object.
+ *    So that both node and obj are point to the same object.
+ *
+ * Example1:
+ * Java:
+ *     Rectangle rec = new Rectangle(3, 4);
+ *     rec.getArea(); // should get 12，3*4=12
+ *
+ * Python:
+ *     rec = Rectangle(3, 4)
+ *     rec.getArea()
+ *
+ * Example2:
+ * Java:
+ *     Rectangle rec = new Rectangle(4, 4);
+ *     rec.getArea(); // should get 16，4*4=16
+ *
+ * Python:
+ *     rec = Rectangle(4, 4)
+ *     rec.getArea()
+ *
  */
 
 // Definition of Node
@@ -30,7 +51,6 @@ public class Solution {
             node = new Node(obj.val);
             System.out.println("Copy the value from obj");
         }
-
     }
 
     public void copyReference(Node obj) {
